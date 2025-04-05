f7adac03b67ccc37976193f0c9cb3f2bc8bac16c
Bionor <maksim16854@gmail.com>
2025-04-05 12:39:57 +0300

    Add files via upload

--- casino_project/casino_project/settings.py
+++ casino_project/casino_project/settings.py
@@ -79,9 +79,6 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
